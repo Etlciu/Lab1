ba39bbfa97cfdfeaa4d08e21bca74c35c5783d39
Etlciu <31518005+Etlciu@users.noreply.github.com>
2017-08-31 13:12:44 -0500

    Add files via upload

---
+++ Lab1Project/src/Lab1.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+	public class Lab1 extends JApplet
+	{ public JButton btnStop = new JButton("STOP");
+		public void init() 
+		{
+			setLayout(new FlowLayout() );
+			add(btnStop);
+		}
+	}
+	
